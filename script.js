db7d5ed6c9781718c8de6fec5de3f2098bbb0fc8
juchi <jenninduka90@gmail.com>
2026-04-01 23:13:33 +0100

    Move to marker on click

--- script.js
+++ script.js
@@ -11,11 +11,13 @@ const inputElevation = document.querySelector('.form__input--elevation');
 class Workout {
   date = new Date();
   id = (Date.now() + '').slice(-10);
+  clicks = 0;
 
   constructor(coords, distance, duration) {
     this.coords = coords; //[lat, lng]
     this.distance = distance; // in km
     this.duration = duration; // in min
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this))
   }
 
   _setDescription() {
@@ -23,6 +25,10 @@ class Workout {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
             'September', 'October', 'November', 'December'];
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+  }
+
+  click() {
+    this.clicks++;
   }
 }
 
@@ -64,6 +70,7 @@ class Cycling extends Workout {
 ///////////////////APPLICATION ARCHITECTURE///////////////////
 class App {
   #map;
+  #mapZoomLevel = 15;
   #mapEvent;
   #workouts = [];
   constructor() {
@@ -92,7 +99,7 @@ class App {
 
     const coords = [latitude, longitude];
 
-    this.#map = L.map('map').setView(coords, 15);
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
@@ -245,6 +252,25 @@ class App {
     }
 
     form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToPopup(e){
+    const workoutEl = e.target.closest('.workout');
+
+    if(!workoutEl) return
+
+    const workout = this.#workouts.find
+    (work => work.id === workoutEl.dataset.id);
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+        animate: true,
+        pan: {
+            duration: 1
+        }
+    });
+
+    //using the public interface
+    workout.click();
   }
 }
 
